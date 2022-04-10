e591e38e14d2a0d1f1917bbe63a9d80aad25014a
Daniexp <dani00exp@gmail.com>
2022-04-10 20:22:50 +0100

    add includes

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: dexposit <dexposit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 17:36:25 by dexposit          #+#    #+#             */
-/*   Updated: 2022/04/10 18:55:50 by dexposit         ###   ########.fr       */
+/*   Updated: 2022/04/10 20:02:07 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define PIPEX_H
 # include "../src/libft/libft.h"
 # include <fcntl.h>
+# include <sys/wait.h>
+# include <string.h>
 
 #endif
